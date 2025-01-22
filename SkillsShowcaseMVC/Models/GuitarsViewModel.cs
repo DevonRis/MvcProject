2181783f37733fc3457db173cd2852145da56ca1
Devon Rismay <drismay@energyogre.com>
2025-01-22 15:37:40 -0600

    GuitarUIData

---
+++ SkillsShowcaseMVC/Models/GuitarsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SkillsShowcase.Shared.Domain.Models.Enums;
+
+namespace SkillsShowcaseMVC.Models
+{
+    public class GuitarsViewModel
+    {
+        public int GuitarId { get; set; }
+        public GuitarOptions GuitarManufacturer { get; set; }
+        public string? GuitarModel { get; set; }
+        public double? GuitarPrice { get; set; }
+        public DateTime? BuildYear { get; set; }
+    }
+}
