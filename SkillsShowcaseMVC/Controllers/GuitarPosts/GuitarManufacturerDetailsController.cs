8d9a228d7587c2c8fae5a6e5c3eac1000ffa22ee
Devon Rismay <drismay@energyogre.com>
2025-01-22 21:40:47 -0600

    GuitarMFDetails Part 1

--- SkillsShowcaseMVC/Controllers/GuitarPosts/GuitarManufacturerDetailsController.cs
+++ SkillsShowcaseMVC/Controllers/GuitarPosts/GuitarManufacturerDetailsController.cs
@@ -1,18 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SkillsShowcase.Shared.Domain.Clients;
+using SkillsShowcase.Shared.Domain.Models.ApiModelsForApiCall;
+using SkillsShowcaseMVC.Models;
 
 namespace SkillsShowcaseMVC.Controllers.GuitarPosts
 {
     public class GuitarManufacturerDetailsController : Controller
     {
         private readonly GetApiClient? _getGuitarMFDetailsAPI;
-        public GuitarManufacturerDetailsController(GetApiClient getApiClient) 
+        public GuitarManufacturerDetailsController(GetApiClient getApiClient)
         {
             _getGuitarMFDetailsAPI = getApiClient;
         }
-        public IActionResult GetGuitarManufacturerDetails(int guitarID)
+        public IActionResult GetGuitarManufacturerDetails(int guitarId)
         {
-            return View();
+            GuitarManufacturerDetailsViewModel? guitarManufacturerDetails = new GuitarManufacturerDetailsViewModel() { };
+            List<GuitarManufactureDetailsForApiCall>? guitarManufacturerDetailsFromAPI = _getGuitarMFDetailsAPI?.GetManufactureDetails().Result;
+            if (guitarManufacturerDetailsFromAPI != null)
+            {
+                var guitarManufacturerDetail = guitarManufacturerDetailsFromAPI.FirstOrDefault(g => g.GuitarManufacturerId == guitarId);
+                if (guitarManufacturerDetail != null)
+                {
+                    guitarManufacturerDetails = new GuitarManufacturerDetailsViewModel
+                    {
+                        GuitarManufacturerId = guitarManufacturerDetail.GuitarManufacturerId,
+                        ManufacturerName = guitarManufacturerDetail.ManufacturerName,
+                        Location = guitarManufacturerDetail.Location,
+                        ContactNumber = guitarManufacturerDetail.ContactNumber,
+                        Email = guitarManufacturerDetail.Email,
+                        Website = guitarManufacturerDetail.Website,
+                        DateEstablished = guitarManufacturerDetail.DateEstablished
+                    };
+                }
+            }
+            return View("~/Views/GuitarManufacturerDetails/_GuitarMFDetails.cshtml", guitarManufacturerDetails);
         }
     }
 }
